7a46d1f3e8693d1ee88d0a7dde1b3ad7b6a8ce17
bebora <simone.rizzo@live.com>
2019-10-04 22:59:18 +0200

    Update Python Telegram Bot to version 12 and move token to external file

--- wahBot/setup.py
+++ wahBot/setup.py
@@ -1,28 +1,33 @@
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import json
 
 
-def start(bot, update):
-    bot.send_message(update.message.chat_id, text='WAH')
+def start(update, context):
+    context.bot.send_message(update.message.chat_id, text='WAH')
 
 
-def answer(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='WAH')
+def answer(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text='WAH')
 
 
-def unknown(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that WAH! Speak clearly, bitch!")
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id,
+                     text='Sorry, I didn\'t understand that WAH! Speak clearly, bitch!')
 
 
 def main():
-    ciccia = telegram.Bot(token='123456789:ASDFGHJKLÒZXCVBNMQWcdByHoYDTsMnxDY') # Sample Token
+    with open('config.json', 'r') as fp:
+        config = json.load(fp)
+    ciccia = telegram.Bot(token=config['token'])
     print(ciccia.get_me())
 
-    updater = Updater(token='123456789:ASDFGHJKLÒZXCVBNMQWcdByHoYDTsMnxDY') # Sample Token
+    updater = Updater(token=config['token'], use_context=True)
 
     dispatcher = updater.dispatcher
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
 
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
